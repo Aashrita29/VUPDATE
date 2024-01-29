fe962844cf7275d9d4b469565354d17ee6d58890
Aashrita29 <113920009+Aashrita29@users.noreply.github.com>
2024-01-29 22:05:36 +0530

    Add files via upload

---
+++ scriptv.js
@@ -0,0 +1,6 @@
+document.getElementById("yesbutton").addEventListener("click",function() {
+    alert("Yes button clicked!");
+});
+document.getElementById("nobutton").addEventListener("click",function() {
+    alert("No button clicked!");
+});
